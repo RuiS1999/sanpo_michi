fb88cee28bed78157f40b90f7f7b172b8cf65945
EC2 Default User <ec2-user@ip-172-31-7-97.ap-northeast-1.compute.internal>
2022-11-08 14:13:13 +0000

    simple_calender導入

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -1,5 +1,5 @@
 class User::PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!, except: [:show, :index, :hashtag]
 
   def new
     @post = Post.new
