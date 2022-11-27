90af8f5c8a66d32854fcc3cedabe1f717d26b85e
EC2 Default User <ec2-user@ip-172-31-7-97.ap-northeast-1.compute.internal>
2022-11-27 05:40:30 +0000

    デプロイ後error_2022.11.27 ver.05

--- app/controllers/user/calorie_calculations_controller.rb
+++ app/controllers/user/calorie_calculations_controller.rb
@@ -1,7 +1,7 @@
 class User::CalorieCalculationsController < ApplicationController
   before_action :authenticate_user!
 
-  def new
+  def result
     @calorie_calculation = CalorieCalculation.new(calorie_calculation_params)
     if @calorie_calculation.mets.blank? || @calorie_calculation.weight.blank? || @calorie_calculation.walking_time.blank? || @calorie_calculation.mets * @calorie_calculation.weight * @calorie_calculation.walking_time * 1.05 == 0
       @calorie = "正しい数値を入力してください"
